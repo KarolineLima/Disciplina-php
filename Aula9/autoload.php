1025057e6bd34f49c73c2079c08b44995aab56e9
Karoline Lima <karol.lima012@gmail.com>
2019-09-21 11:25:21 -0300

    Atualização-Aula 9

--- Aula9/autoload.php
+++ Aula9/autoload.php
@@ -1,24 +1,19 @@
 <?php
+    spl_autoload_register(function ($classe){
+        echo "Procurando arquivo $classe.php na 1a funçao";
+        if(file_exists("$classe.php"))
+            require_once("$classe.php");
+    });
 
+    spl_autoload_register(function ($classe){
+        echo "Procurando arquivo $classe.php no 2a função";
+        if(file_exists("bixo/$classe.php"))
+            require_once("bixo/$classe.php");
+    });
 
-
-spl_autoload_register(function ($classe){
-    echo 'Procurando arquivo $classe.php na 1a função';
-    if(file_exists("$classe.php"))
-        require_once("$classe.php");
-});
-
-spl_autoload_register(function ($classe){
-    echo 'Procurando arquivo $classe.php na 2a função';
-        require_once("bixo/$classe.php");
-});
-
-
-
-
-function f($classe){
-    echo 'Procurando arquivo $classe.php na 1a função';
-    require_once("$classe.php");
-}
-
+    spl_autoload_register(function ($classe){
+        echo "Procurando arquivo $classe.php no 3a função";
+        if(file_exists("caracteristicas/$classe.php"))
+            require_once("caracteristicas/$classe.php");
+    });
 ?>
