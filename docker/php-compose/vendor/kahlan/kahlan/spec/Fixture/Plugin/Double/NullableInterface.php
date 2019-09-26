d02cf945b146433fbd25947b49b2f0e0bf690162
Karoline Lima <karol.lima012@gmail.com>
2019-09-26 11:52:50 -0300

    Atualização Repositório

--- docker/php-compose/vendor/kahlan/kahlan/spec/Fixture/Plugin/Double/NullableInterface.php
+++
@@ -1,8 +0,0 @@
-<?php
-namespace Kahlan\Spec\Fixture\Plugin\Double;
-
-interface NullableInterface
-{
-    public function foo(?int $integer = null): ?int;
-    public function doz(?\Kahlan\Spec\Fixture\Plugin\Double\Doz $instance = null): ?\Kahlan\Spec\Fixture\Plugin\Double\Doz;
-}
